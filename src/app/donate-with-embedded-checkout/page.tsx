c0bbb3393e7030878ff5e4cab48ae962d1287138
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2024-11-24 20:30:18 +0530

    feat: donate with embedded checkout layout

---
+++ src/app/donate-with-embedded-checkout/page.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+
+import CheckoutForm from '@/components/CheckoutForm';
+
+export const metadata: Metadata = {
+  title: 'Donate with embedded Checkout | Next.js + TypeScript Example',
+};
+
+const DonateWithEmbeddedCheckout = (): JSX.Element => {
+  return (
+    <div className='page-container'>
+      <h1>Donate with embedded Checkout</h1>
+      <p>Donate to our project 💖</p>
+      <CheckoutForm uiMode='embedded' />
+    </div>
+  );
+};
+
+export default DonateWithEmbeddedCheckout;
